2fe83c35efb18353fbea641bf04c56debb9247d6
Almestin <davottakoyadres@gmail.com>
2026-03-05 13:38:30 +0100

    added some commands

--- handlers/routes.py
+++ handlers/routes.py
@@ -8,6 +8,23 @@ router = Router()
 async def start(message: Message):
     await message.answer(f"Hello {message.from_user.first_name}")
 
+@router.message(Command("random"))
+async def random(message: Message):
+    await message.answer(f"This is a randon fact")
+
+@router.message(Command("gpt"))
+async def gpt(message: Message):
+    await message.answer(f"This is a gpt")
+
+@router.message(Command("talk"))
+async def talk(message: Message):
+    await message.answer(f"This is a talk")
+
+@router.message(Command("quiz"))
+async def quiz(message: Message):
+    await message.answer("This is a quiz")
+
+
 @router.message()
 async def messages(message: Message):
     await message.answer("<b>Some text</b>", parse_mode="HTML")
